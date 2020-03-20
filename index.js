5023383eb1f4f3153f92789a44d823ea848ba232
Aleksandras <aleksxx98@gmail.com>
2020-03-21 00:36:18 +0200

    redesign of navbar, eater egg hidden, repositioned about section

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log('%c12', 'margin:0; font-size:20vh; background: url("https://cdn.discordapp.com/attachments/181807503087108096/326353443625107456/TRIGGERED.gif") 0 0; background-size:cover; background-repeat:no-repeat; padding:100px; color:transparent; opacity:0; margin:auto;');
+checkbox = document.querySelector('.navCheck');
+checkbox.addEventListener('click', () => {
+  if (checkbox.checked)
+    console.log('%c12', 'margin:0; font-size:20vh; background: url("https://cdn.discordapp.com/attachments/181807503087108096/326353443625107456/TRIGGERED.gif") 0 0; background-size:cover; background-repeat:no-repeat; padding:100px; color:transparent; opacity:0; margin:auto;');
+
+}, { once: true });
 console.log('%c WHAT ARE YOU DOING HERE?!', 'font-weight:bold; font-size:50px; color:red; text-shadow: 1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0px black; text-align:center;');
