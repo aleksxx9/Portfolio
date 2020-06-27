04dee3110b1e32a709b924277d69529386af77d1
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-06-27 18:21:00 +0300

    moved data to json

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-checkbox = document.querySelector('.navCheck');
-checkbox.addEventListener('click', () => {
-  if (checkbox.checked)
-    console.log('%c12', 'margin:0; font-size:20vh; background: url("https://cdn.discordapp.com/attachments/181807503087108096/326353443625107456/TRIGGERED.gif") 0 0; background-size:cover; background-repeat:no-repeat; padding:100px; color:transparent; opacity:0; margin:auto;');
-
-}, { once: true });
-console.log('%c WHAT ARE YOU DOING HERE?!', 'font-weight:bold; font-size:50px; color:red; text-shadow: 1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0px black; text-align:center;');
+fetch("./data.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
